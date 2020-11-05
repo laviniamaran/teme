93e6aeec8aa3fb3a104a4a56a13b53119dc48be0
Maran Lavinia <laviniamaran@gmail.com>
2020-11-05 23:00:39 +0200

    add tema 2

--- tema2.py
+++ tema2.py
@@ -1,4 +1,4 @@
 print((False or (False and True) or True)==(False or False and True or True))
-# print(False or False) and (True or True)
+print((False or False) and (True or True)==(False or False and True or True))
 # print(False or (False and True) or True)
 # print(False or False and True or True)
